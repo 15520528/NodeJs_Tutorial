bce6f95330e742998766d36c1ce315970effe1fc
nam <namvh3@vng.com.vn>
2019-02-18 11:16:33 +0700

    done Mongo_DB

---
+++ Mongo_DB/update_one.js
@@ -0,0 +1,15 @@
+var config = require('./config')
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("namvh3");
+    var myquery = { address: "Highway 39" };
+    var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
+    dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      db.close();
+    });
+  });
